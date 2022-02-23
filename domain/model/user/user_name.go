4207134fbdf6c1fd801efc853bd663a4ab3e9069
mizuki-n-2 <mizukizhangze@gmail.com>
2022-02-23 15:04:23 +0900

    fix: typo

--- domain/model/user/user_name.go
+++ domain/model/user/user_name.go
@@ -9,11 +9,11 @@ import (
 type UserName string
 
 func NewUserName(value string) (UserName, error) {
-	MIN_LENGTH_USER_BIO := 2
-	MAX_LENGTH_USER_BIO := 20
+	MIN_LENGTH_USER_NAME := 2
+	MAX_LENGTH_USER_NAME := 20
 
-	if utf8.RuneCountInString(value) < MIN_LENGTH_USER_BIO || utf8.RuneCountInString(value) > MAX_LENGTH_USER_BIO {
-		return "", fmt.Errorf("nameは%d文字以上%d文字以下にしてください", MIN_LENGTH_USER_BIO, MAX_LENGTH_USER_BIO)
+	if utf8.RuneCountInString(value) < MIN_LENGTH_USER_NAME || utf8.RuneCountInString(value) > MAX_LENGTH_USER_NAME {
+		return "", fmt.Errorf("nameは%d文字以上%d文字以下にしてください", MIN_LENGTH_USER_NAME, MAX_LENGTH_USER_NAME)
 	}
 
 	return UserName(value), nil
